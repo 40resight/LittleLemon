73fe7cd58cbfb15c82d392b7421ff09a61b44cb4
bunamin <bunamin@littlelemon.com>
2023-06-24 09:50:51 +0100

    24/6 update

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
+class Booking(models.Model):
+    ID = models.IntegerField(validators=[MaxValueValidator(11)])
+    Name = models.CharField(max_length = 255)
+    no_of_guests = models.IntegerField(validators=[MaxValueValidator(20)])
+    BookingDate = models.DateTimeField(auto_now_add=True)
+
+class Menu(models.Model):
+    ID = models.IntegerField(validators=[MaxValueValidator(5)])
+    Title = models.CharField(max_length = 255)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.IntegerField(validators=[MaxValueValidator(11)])
--- Restaurant/views.py
+++ Restaurant/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .models import Menu
+from rest_framework import viewsets
+from .models import Booking
+from .serializers import BookingSerializer
+from .serializers import MenuSerializer
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateAPIView,
+    DestroyAPIView,
+)
 
-# Create your views here.
-def index(request):
-    return render(request, 'index.html', {})
+class MenuView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    pass
+
+class SingleMenuView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    pass
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
